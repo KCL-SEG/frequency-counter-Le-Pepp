d7d1e617c8de887a2e2c47606359e10b349d78ca
Pepp <agamer743@gmail.com>
2022-10-07 12:36:45 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    i = 0
+    while i < len(items):
+        items[i] = str(items[i])
+        i += 1
+    
     frequencies = {}
-    # Your code goes here
+    i = 0
+    while i < len(items):
+        x = items[i]
+        j = 0
+        repeats = 0
+        while j < len(items):
+            if x == items[j]:
+                repeats += 1
+            j += 1
+        frequencies[items[i]] = repeats
+        i += 1
     return frequencies
+
